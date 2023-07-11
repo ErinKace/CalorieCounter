de77c5bcc714ae7bd3eaae7d4eeadc13eeb3d5ab
ErinKace <erinkacerovskis@gmail.com>
2023-07-11 15:51:39 -0500

    ran into a wall with ingredient vs ingredientType

--- src/main/java/org/launchcode/CalorieCounter/Ingredient.java
+++ src/main/java/org/launchcode/CalorieCounter/Ingredient.java
@@ -1,4 +1,71 @@
 package org.launchcode.CalorieCounter;
 
-public class Ingredient {
+public class Ingredient implements CalorieConversion {
+    private String name;
+    private double quantityInOunces;
+    private double quantityInGrams;
+    private double caloriesPerOunce;
+    private double caloriesPerGram;
+    private double proteinPerGram;
+    private double sugarPerGram;
+    private double fatPerGram;
+    private double fiberPerGram;
+
+    public Ingredient(String name, double quantity, String quantityType, double calories) {
+        this.name = name;
+        if (quantityType.equals("g")) {
+            this.quantityInGrams = quantity;
+            this.quantityInOunces = CalorieConversion.convertGramsToOunces(quantity);
+        } else if (quantityType.equals("oz")) {
+            this.quantityInOunces = quantity;
+            this.quantityInGrams = CalorieConversion.convertOuncesToGrams(quantity);
+        }
+        this.caloriesPerGram = calories/quantityInGrams;
+        this.caloriesPerOunce = calories/quantityInOunces;
+    }
+    public Ingredient(String name, double quantity, String quantityType, double calories, double protein, double sugar, double fat, double fiber) {
+        this(name, quantity, quantityType, calories);
+        this.proteinPerGram = protein/quantityInGrams;
+        this.sugarPerGram = sugar/quantityInGrams;
+        this.fiberPerGram = fiber/quantityInGrams;
+        this.fatPerGram = fat/quantityInGrams;
+    }
+
+    @Override
+    public double getCaloriesPerGram() {
+        return quantityInGrams*caloriesPerGram;
+    }
+
+    @Override
+    public double getCaloriesPerOunce() {
+        return quantityInOunces*caloriesPerOunce;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getQuantityInOunces() {
+        return quantityInOunces;
+    }
+
+    public double getQuantityInGrams() {
+        return quantityInGrams;
+    }
+
+    public double getProteinPerGram() {
+        return proteinPerGram;
+    }
+
+    public double getSugarPerGram() {
+        return sugarPerGram;
+    }
+
+    public double getFatPerGram() {
+        return fatPerGram;
+    }
+
+    public double getFiberPerGram() {
+        return fiberPerGram;
+    }
 }
